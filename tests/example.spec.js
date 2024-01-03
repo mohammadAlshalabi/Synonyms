0643d330fdeb81dd5db0fbf7818343a554f12e00
mohammadAlshalabi <m.alshalabi96@gmail.com>
2024-01-03 23:08:49 +0200

    add playwright

---
+++ tests/example.spec.js
@@ -0,0 +1,20 @@
+const { test } = require('@playwright/test');
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+  await page.route('**/*', route => route.continue());
+
+  await page.locator('[id="synonymsInput"]').fill('bird');
+  await page.locator('[data-testid="Submit"]').click();
+  const response = await page.waitForResponse(response => response.url().includes('https://api.datamuse.com/words'));
+  
+  const responseBody = await response.text();
+  const elements = await page.$$('[data-testId*="synonyms"]');
+
+  const elementActions = elements.map(async (element, index) => {
+    const textContent = await element.textContent();
+    test.expect(textContent).toStrictEqual(responseBody[index].word)
+  });
+
+  await Promise.all(elementActions);
+});
